35184ebe4456bc9dff4dda14a03bfc4253594b79
ryanfeatherman <ryan.featherman3@gmail.com>
2020-04-06 23:37:52 -0700

    added constructor and getMoves stub for bishop (forgotten)

--- src/java/Bishop.java
+++ src/java/Bishop.java
@@ -1,5 +1,19 @@
 package java;
 
+import java.util.Set;
+
 public class Bishop extends Piece {
-    
+
+    public Bishop(Square s, Color c, Board b) {
+        super(s, c, b);
+    }
+
+    public Bishop(int x, int y, Color c, Board b) {
+        super(x, y, c, b);
+    }
+
+    @Override
+    public Set<Square> getMoves() {
+        return null;
+    }
 }
